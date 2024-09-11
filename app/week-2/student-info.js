3bf31cb4a817e978acfe4a5de3553ef5447ee73f
Zaid Ahmad <zaidd250@gmail.com>
2024-09-11 09:48:51 -0600

    complete week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h2>Zaid Ahmad</h2>
+            <Link href={""}>Link to GitHub repository</Link>
+        </div>
+    );
+}
