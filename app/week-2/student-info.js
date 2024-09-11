8e7e8d2ec8b1e828bf068784228edc989c21bb6f
Zaid Ahmad <zaidd250@gmail.com>
2024-09-11 09:51:18 -0600

    feat: Update GitHub repository link in StudentInfo component

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,9 @@ export default function StudentInfo() {
     return (
         <div>
             <h2>Zaid Ahmad</h2>
-            <Link href={""}>Link to GitHub repository</Link>
+            <Link href={"https://github.com/zaid-ahmad/cprg306-assignments"}>
+                Link to GitHub repository
+            </Link>
         </div>
     );
 }
