211a93387d9f81ee35d820c40d6c6387d4e84d07
Zaid Ahmad <zaidd250@gmail.com>
2024-09-18 08:30:50 -0600

    chore: Update fonts and layout styles

--- app/page.js
+++ app/page.js
@@ -2,9 +2,20 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div>
-            <h1>CPRG 306: Web Development 2 - Assignments</h1>
-            <Link href={"/week-2"}>Week 2</Link>
-        </div>
+        <main className='flex flex-col items-center justify-center h-screen'>
+            <h1 className='text-3xl font-bold'>
+                CPRG 306: Web Development 2 - Assignments
+            </h1>
+            <ul className='mt-5'>
+                <li className='text-lg'>
+                    <Link
+                        href={"/week-2"}
+                        className='transition-all px-8 py-2 rounded hover:bg-zinc-800 focus:bg-zinc-700'
+                    >
+                        Week 2
+                    </Link>
+                </li>
+            </ul>
+        </main>
     );
 }
