8109e42ef91d2a290318bb136127a79248a05482
Zaid Ahmad <zaidd250@gmail.com>
2024-09-27 10:09:34 -0600

    week 4: Complete part 1

--- app/page.js
+++ app/page.js
@@ -24,6 +24,15 @@ export default function Home() {
                         Week 3
                     </Link>
                 </li>
+
+                <li>
+                    <Link
+                        href={"/week-4"}
+                        className='transition-all px-8 py-2 rounded hover:bg-zinc-800 focus:bg-zinc-700'
+                    >
+                        Week 4
+                    </Link>
+                </li>
             </ul>
         </main>
     );
